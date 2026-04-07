777463796c3310f8c4ba71235eed03e756ca4d20
ZhongliangHuang <201906080109@zjut.edu.cn>
2026-04-07 11:39:54 +0800

    Delete ddvpp_matlab_revised directory

--- ddvpp_matlab_revised/ddvpp_pack_x.m
+++
@@ -1,5 +0,0 @@
-function x = ddvpp_pack_x(data)
-    m = data.dyn(:,3);
-    d = data.dyn(:,4);
-    x = [m(data.ibr_idx); d(data.ibr_idx)];
-end
--- ddvpp_matlab_revised/ddvpp_pack_x_current.m
+++
@@ -1,5 +0,0 @@
-function x = ddvpp_pack_x_current(model, data)
-    m = model.m;
-    d = model.d;
-    x = [m(data.ibr_idx); d(data.ibr_idx)];
-end
